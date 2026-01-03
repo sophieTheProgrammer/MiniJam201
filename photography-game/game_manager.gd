8bc3171e9a22ed1e5d828a411e79cfcd915782af
Sophie <sophie.thi.nguyen11@gmail.com>
2026-01-02 17:52:27 -0800

    added function spawn_flower()

--- photography-game/game_manager.gd
+++ photography-game/game_manager.gd
@@ -2,15 +2,20 @@ extends Node
 
 const flowerFab = preload("res://flower.tscn")
 # Called when the node enters the scene tree for the first time.
-var row = 5
 func _ready() -> void:
-	for i in range(3):
+	spawn_flower()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func spawn_flower():
+	var row = 5
+	var flowers_array = []
+	
+	for i in range(10):
 		var curr = flowerFab.instantiate()
 		curr.name = "flower"
 		curr.position.x = row
 		row += 100
 		self.add_child(curr)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
