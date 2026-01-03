81ee755aadbc3d5add7ef14ff7fc94a8dbe99ef4
Sophie <sophie.thi.nguyen11@gmail.com>
2026-01-03 14:17:27 -0800

    aestic changes and balances

--- photography-game/scripts/shop.gd
+++ photography-game/scripts/shop.gd
@@ -21,10 +21,7 @@ func _on_film_btn_pressed() -> void:
 	if (Global.moneys >= Global.film_cost):
 		Global.film_amount += 2
 		Global.moneys -= Global.film_cost
-		if (Global.film_cost <= 10):
-			Global.film_cost *= 1.211
-			Global.bfly_cost *= 0.7
-			Global.fert_cost *= 0.9
+		Global.film_cost *= 1.211
 		buy_sound.play()
 
 func _on_butterfly_btn_pressed() -> void:
@@ -32,9 +29,7 @@ func _on_butterfly_btn_pressed() -> void:
 		Global.butterfly_spawn_count = int(Global.butterfly_spawn_count*1.25)
 		print("butterfly spawning ", str(Global.butterfly_spawn_count))
 		Global.moneys -= Global.bfly_cost
-		Global.film_cost *= 0.9
 		Global.bfly_cost *= 3.111
-		Global.fert_cost *= 0.7
 		buy_sound.play()
 
 func _on_fert_btn_pressed() -> void:
@@ -42,8 +37,6 @@ func _on_fert_btn_pressed() -> void:
 		Global.flower_spawn_count = int(Global.flower_spawn_count*1.25)
 		print("flower_spawning " + str(Global.flower_spawn_count))
 		Global.moneys -= Global.fert_cost
-		Global.film_cost *= 0.7
-		Global.bfly_cost *= 0.9
 		Global.fert_cost *= 2.111
 		buy_sound.play()
 
