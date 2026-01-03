f9e27d52c91fffc5e64935701e953c7abdb26492
andreaTheCoder <105570399+andreaTheCoder@users.noreply.github.com>
2026-01-02 18:06:50 -0800

    globals with arrary working
    also remaned and moved around some files

--- photography-game/scripts/game_manager.gd
+++ photography-game/scripts/game_manager.gd
@@ -1,17 +1,12 @@
 extends Node
 
-const flowerFab = preload("res://flower.tscn")
+const flowerFab = preload("res://scenes/flower.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	spawn_flower()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 func spawn_flower():
-	var row = 5
-	var flowers_array = []
+	var row = 0
 	
 	for i in range(10):
 		var curr = flowerFab.instantiate()
@@ -19,7 +14,8 @@ func spawn_flower():
 		curr.position.x = row
 		row += 100
 		self.add_child(curr)
-
+		Global.frame_items.append(curr)
+	print(Global.frame_items)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_released("click"):
