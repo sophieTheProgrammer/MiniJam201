ae84ba5e47f3f9e7bb07d21494fbf34a1ad7c0a7
andreaTheCoder <105570399+andreaTheCoder@users.noreply.github.com>
2026-01-02 17:54:44 -0800

    made another frame img
    z index and remaned some stuff

--- photography-game/game_manager.gd
+++ photography-game/game_manager.gd
@@ -13,4 +13,5 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_released("click"):
+		print("Left mouse button released.")
