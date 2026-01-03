dccabbbbec9d561c5e6f9ac5f2935dfa53de897c
Jasper-Cn <leshan.jasper@gmail.com>
2026-01-03 13:31:56 -0800

    Shop changing prices based on market demand

--- photography-game/scripts/global.gd
+++ photography-game/scripts/global.gd
@@ -7,9 +7,12 @@ enum FrameTypes{
 var x_viewport_length
 var y_viewport_length
 var film_amount = 1
-var moneys = 0
+var moneys = 100
 var flower_spawn_count = 30
 var butterfly_spawn_count = 10
+var film_cost = 1
+var bfly_cost = 20
+var fert_cost = 5
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
--- photography-game/scripts/shop.gd
+++ photography-game/scripts/shop.gd
@@ -1,33 +1,47 @@
 extends Sprite2D
-var film_cost = 20
-var bfly_cost = 20
-var fert_cost = 20
 var game_scene:PackedScene = load("res://scenes/game.tscn")
+@onready var film_btn: Button = $"../film upgrade/film btn"
+@onready var bfly_btn: Button = $"../one with the butterflies upgrade/butterfly btn"
+@onready var fert_btn: Button = $"../fertilizer/fert btn"
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	Global.film_cost = int(ceil(Global.film_cost))
+	Global.bfly_cost = int(ceil(Global.bfly_cost))
+	Global.fert_cost = int(ceil(Global.fert_cost))
+	film_btn.text = "$" + str(Global.film_cost)
+	bfly_btn.text = "$" + str(Global.bfly_cost)
+	fert_btn.text = "$" + str(Global.fert_cost)
 
 func _on_film_btn_pressed() -> void:
-	if (Global.moneys >= film_cost):
+	if (Global.moneys >= Global.film_cost):
 		Global.film_amount += 1
-		Global.moneys -= film_cost
-
-func _on_fert_btn_pressed() -> void:
-	if (Global.moneys >= fert_cost):
-		Global.flower_spawn_count = int(Global.flower_spawn_count*1.25)
-		print("flower_spawning " + str(Global.flower_spawn_count))
-		Global.moneys -= fert_cost
+		Global.moneys -= Global.film_cost
+		Global.film_cost *= 1.211
+		Global.bfly_cost *= 0.7
+		Global.fert_cost *= 0.9
 
 func _on_butterfly_btn_pressed() -> void:
-	if (Global.moneys >= bfly_cost):
+	if (Global.moneys >= Global.bfly_cost):
 		Global.butterfly_spawn_count = int(Global.butterfly_spawn_count*1.25)
 		print("butterfly spawning ", str(Global.butterfly_spawn_count))
-		Global.moneys -= bfly_cost
+		Global.moneys -= Global.bfly_cost
+		Global.film_cost *= 0.9
+		Global.bfly_cost *= 3.111
+		Global.fert_cost *= 0.7
 
+func _on_fert_btn_pressed() -> void:
+	if (Global.moneys >= Global.fert_cost):
+		Global.flower_spawn_count = int(Global.flower_spawn_count*1.25)
+		print("flower_spawning " + str(Global.flower_spawn_count))
+		Global.moneys -= Global.fert_cost
+		Global.film_cost *= 0.7
+		Global.bfly_cost *= 0.9
+		Global.fert_cost *= 2.111
 
 func _on_exit_shop_pressed() -> void:
 	get_tree().change_scene_to_packed(game_scene)
