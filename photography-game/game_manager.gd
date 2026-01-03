52d6c95284e2ccb68823b53e18f8fda3963e912e
Sophie <sophie.thi.nguyen11@gmail.com>
2026-01-02 18:09:12 -0800

    randomly spawning flowers :D

--- photography-game/game_manager.gd
+++ photography-game/game_manager.gd
@@ -3,19 +3,28 @@ extends Node
 const flowerFab = preload("res://flower.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	spawn_flower()
+	spawn_flower(10)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-func spawn_flower():
+# spawns flowers in random location in certain range from origin
+func spawn_flower(number_of_flowers):
+	
+	# set variables
 	var row = 5
 	var flowers_array = []
-	
-	for i in range(10):
+
+	# set the length of the viewport here when it is changed to panning
+	var x_viewport_length = 1152
+	var y_viewport_length = 648
+
+	for i in range(number_of_flowers):
 		var curr = flowerFab.instantiate()
 		curr.name = "flower"
-		curr.position.x = row
-		row += 100
+		
+		curr.position.x = (randi() % x_viewport_length) - x_viewport_length/2
+		curr.position.y = (randi() % y_viewport_length) - y_viewport_length/2
 		self.add_child(curr)
+		
