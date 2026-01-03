7ee842bf9b963c780efd527514e25e4e29197ed6
Jasper-Cn <leshan.jasper@gmail.com>
2026-01-02 19:39:49 -0800

    butterfly

--- photography-game/scripts/game_manager.gd
+++ photography-game/scripts/game_manager.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
 const flowerFab = preload("res://scenes/flower.tscn")
+const butterflyFab = preload("res://scenes/butterfly.tscn")
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	spawn_flower(10)
-
+	spawn_butterfly(3)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_released("click"):
@@ -28,16 +31,27 @@ func spawn_flower(number_of_flowers):
 	var row = 5
 	var flowers_array = []
 
-	# set the length of the viewport here when it is changed to panning
-	var x_viewport_length = 1152
-	var y_viewport_length = 648
-
 	for i in range(number_of_flowers):
 		var curr = flowerFab.instantiate()
 		curr.name = "flower"
 		
-		curr.position.x = (randi() % x_viewport_length) - x_viewport_length/2
-		curr.position.y = (randi() % y_viewport_length) - y_viewport_length/2
+		curr.position.x = (randi() % Global.x_viewport_length) - Global.x_viewport_length/2
+		curr.position.y = (randi() % Global.y_viewport_length) - Global.y_viewport_length/2
+		self.add_child(curr)
+		Global.frame_items.append(curr)
+	print(Global.frame_items)
+# spawns flowers in random location in certain range from origin
+
+func spawn_butterfly(number_of_butterflies):
+	# set variables
+	var row = 5
+	var flowers_array = []
+
+	for i in range(number_of_butterflies):
+		var curr = butterflyFab.instantiate()
+		curr.name = "butterfly"
+		curr.position.x = (randi() % Global.x_viewport_length) - Global.x_viewport_length/2
+		curr.position.y = (randi() % Global.y_viewport_length) - Global.y_viewport_length/2
 		self.add_child(curr)
 		Global.frame_items.append(curr)
 	print(Global.frame_items)
