7655b40661676cbd72643c7bea8d49103e8f774a
Jasper-Cn <leshan.jasper@gmail.com>
2026-01-03 15:21:35 -0800

    Balanced shop??

--- photography-game/scripts/game_manager.gd
+++ photography-game/scripts/game_manager.gd
@@ -18,6 +18,8 @@ func _ready() -> void:
 	spawn_butterfly(Global.butterfly_spawn_count)
 	
 func _process(delta: float) -> void:
+	if Global.moneys < Global.film_cost and Global.film_amount <= 0:
+		get_tree().change_scene_to_packed(game_over)
 	if Input.is_action_just_released("click"):
 		if Global.film_amount > 0:
 			click.play()
@@ -36,9 +38,11 @@ func count_items_in_frame():
 	var flower = flowerFab.instantiate()
 	var butterfly = butterflyFab.instantiate()
 	var mouseRect = Rect2(mousePos.x, mousePos.y, frame.texture.get_width()*frame.transform.get_scale().x, frame.texture.get_height()*frame.transform.get_scale().x)
+	print(mouseRect)
 	for item in frame_items:
 		var tex = item.sprite
 		var itemRect = Rect2(item.position.x, item.position.y, tex.texture.get_width()*tex.transform.get_scale().x, tex.texture.get_height()*tex.transform.get_scale().y)
+		print(itemRect)
 		if mouseRect.intersects(itemRect):
 			if item.type == Global.FrameTypes.BUTTERFLY:
 				count += 2
