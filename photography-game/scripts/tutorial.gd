31712d9df6bd99b83c23a8710be98398ad9c7f32
andreaTheCoder <105570399+andreaTheCoder@users.noreply.github.com>
2026-01-04 18:31:12 -0800

    better tutoiral

--- photography-game/scripts/tutorial.gd
+++ photography-game/scripts/tutorial.gd
@@ -11,39 +11,43 @@ const GAME = preload("res://scenes/game.tscn")
 var tutorial_info = [
 	{
 		"text":"you are an avid nature photographer",
-		"image_path":"res://handdrawn/tutorial/0.png"
-	},
-	{
-		"text":"too bad you are BROKE!!!",
 		"image_path":"res://handdrawn/tutorial/1.png"
 	},
 	{
-		"text":"fortunatly... you have one more film left in your camera",
+		"text":"too bad you are BROKE!!!",
 		"image_path":"res://handdrawn/tutorial/2.png"
 	},
 	{
-		"text":"and people are willing to PAY for cool nature pics!",
+		"text":"unfortunatly... you only have one film left in your camera",
 		"image_path":"res://handdrawn/tutorial/3.png"
 	},
 	{
-		"text":"they love flowers and butterflies",
+		"text":"luckily, people are willing to PAY for cool nature pics!",
 		"image_path":"res://handdrawn/tutorial/4.png"
 	},
 	{
-		"text":"watch out for bugs though...",
+		"text":"they love flowers and butterflies",
 		"image_path":"res://handdrawn/tutorial/5.png"
 	},
 	{
-		"text":"use WASD to find your best shot, and click to shoot",
+		"text":"watch out for bugs though...",
 		"image_path":"res://handdrawn/tutorial/6.png"
 	},
 	{
-		"text":"buy film and upgrades at the shop if you dare",
+		"text":"use WASD or the arrow keys to find your best shot",
 		"image_path":"res://handdrawn/tutorial/7.png"
 	},
 	{
-		"text":"good luck!",
+		"text":"click to shoot your photo once you are ready",
 		"image_path":"res://handdrawn/tutorial/8.png"
+	},
+	{
+		"text":"buy more film and upgrades at the shop",
+		"image_path":"res://handdrawn/tutorial/9.png"
+	},
+	{
+		"text":"good luck!",
+		"image_path":"res://handdrawn/tutorial/10.png"
 	}
 ]
 func _ready() -> void:
@@ -54,13 +58,15 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_released("click"):
-		current_step += 1
-		if current_step == 8:
-			print("current step is 9")
+		if current_step == 10:
+			print("switching to game...")
 			get_tree().change_scene_to_packed(GAME)
+		else:
+			loadTextFromIndex(current_step)
+			loadImageFromIndex(current_step)
+		current_step += 1
 
-		loadTextFromIndex(current_step)
-		loadImageFromIndex(current_step)
+		
 			
 func loadImageFromIndex(index):
 	if not tutorial_info[index]["image_path"]:
